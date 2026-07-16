96b728394731814e1948b7e070fdd565dadd60f6
bhaskarbais29 <bhaskarbais1@gmail.com>
2026-07-16 05:39:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_VALUE",
-  authDomain: "PASTE_YOUR_VALUE",
-  projectId: "PASTE_YOUR_VALUE",
-  storageBucket: "PASTE_YOUR_VALUE",
-  messagingSenderId: "PASTE_YOUR_VALUE",
-  appId: "PASTE_YOUR_VALUE",
+  apiKey: "AIzaSyB-KdpL9UQQ-vPk4WTzsjeU5pFPup_3YTA",
+  authDomain: "body-temple-gyms.firebaseapp.com",
+  projectId: "body-temple-gyms",
+  storageBucket: "body-temple-gyms.firebasestorage.app",
+  messagingSenderId: "592590191444",
+  appId: "1:592590191444:web:0aa47ed8a1a8fcf67ae6c1",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
